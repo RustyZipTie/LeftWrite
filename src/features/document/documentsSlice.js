9cfd5898ef7a0caaf2dbef77751081b97df7564a
RustyZipTie <kaigrignon@gmail.com>
2024-02-17 09:11:33 -0800

    starting on redux stuff

---
+++ src/features/document/documentsSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    documentsArray: [],
+    activeDocument: null
+};
+
+const documentsSlice = createSlice({
+    name: 'documents',
+    initialState,
+    reducers: {
+        newDocument: (state, action) => {
+            state.documentsArray.push({
+                title: 'New Document',
+                nodes: [
+                    {
+                        type: 'text',
+                        content: 'Hello, World'
+                    }
+                ]
+            });
+            console.log('docs arr', state.documentsArray);
+        },
+        setActive: (state, action) => {
+            state.activeDocument = action.payload;
+            console.log('active:', state.activeDocument);
+        },
+        updateActive: (state, action)
+    }
+});
+
+export const selectAllDocuments = () => state.documents.documentsArray;
